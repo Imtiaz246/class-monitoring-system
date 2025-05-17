368ada3d8a3f511f52970753e74c0eafae4b0855
salehine <mushfiq1060@gmail.com>
2025-05-17 15:58:40 +0600

    feat: implement verification email functionality

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -4,6 +4,7 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from "@/db/schema/auth-schema";
 import { admin } from "better-auth/plugins";
 import { ac, roles } from "@/utils/permission";
+import { sendVerificationEmail } from "@/services/email/send-verification-email";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -13,6 +14,24 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     autoSignIn: false,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    expiresIn: 60 * 60,
+    autoSignInAfterVerification: false,
+    sendVerificationEmail: async ({ user, url }) => {
+      const link = new URL(url);
+      await sendVerificationEmail({
+        to: user.email,
+        subject: "Verify your email address",
+        meta: {
+          description:
+            "Please verify your email address to complete the registration process.",
+          link: String(link),
+        },
+      });
+    },
   },
   plugins: [
     admin({
